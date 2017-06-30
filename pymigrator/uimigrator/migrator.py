470431c7d39ebfd97fa534c0e7cadf5fd9e24beb
codyowl <codyowl@gmail.com>
2017-06-30 12:55:44 +0530

    html files finder function added :construction:

--- pymigrator/uimigrator/migrator.py
+++ pymigrator/uimigrator/migrator.py
@@ -1,3 +1,16 @@
+import sys
+import os
+
 class Bootstrapmigrator:
-    def migrate_to(self, version_number):
-        self.version_number = version_number                
+    def project_path(self, path):
+        self.path = path
+        try:
+            os.listdir(self.path)
+            self.files_path = [os.path.join(path, names) for path, subdir, files in os.walk(self.path) for names in files]
+            self.html_files = [files for files in self.files_path if files.endswith('.html')]
+        except IOError as e:
+           raise ValueError (e) 
+    def to_version(self, version_number):
+        self.version_number = version_number
+    def migrate(self):
+        print "your codings are migrating"
